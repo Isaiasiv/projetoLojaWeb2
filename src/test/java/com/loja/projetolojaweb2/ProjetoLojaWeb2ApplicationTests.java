5455d4984987bb7017b5cff2a566bf604fec9b71
Isaiasiv <isaias.ribeiro@estudante.ifgoiano.edu.br>
2024-07-09 17:58:48 -0300

    Initial commit

---
+++ src/test/java/com/loja/projetolojaweb2/ProjetoLojaWeb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loja.projetolojaweb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoLojaWeb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
